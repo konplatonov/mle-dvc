8169b3a4b86a1c7abaa4bc3998c654d65d279053
konplatonov <konplatonov@gmail.com>
2025-07-19 12:54:08 +0000

    обновление DVC-пайплайна

--- scripts/fit.py
+++ scripts/fit.py
@@ -5,6 +5,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from category_encoders import CatBoostEncoder
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LogisticRegression
 from catboost import CatBoostClassifier
 import yaml
 import os
@@ -27,14 +28,14 @@ def fit_model():
     preprocessor = ColumnTransformer(
         [
             ('binary', OneHotEncoder(drop='if_binary'), binary_cat_features.columns.tolist()),
-            ('cat', CatBoostEncoder(return_df=False), other_cat_features.columns.tolist()),
+            ('cat', OneHotEncoder(), other_cat_features.columns.tolist()),
             ('num', StandardScaler(), num_features.columns.tolist())
         ],
         remainder='drop',
         verbose_feature_names_out=False
     )
 
-    model = CatBoostClassifier(auto_class_weights='Balanced')
+    model = LogisticRegression(C=1 , penalty='l2')
 
     pipeline = Pipeline(
         [
